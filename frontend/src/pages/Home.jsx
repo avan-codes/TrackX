ca42b0b3216258b89481a43b7fe8712a6ef99523
avan-codes <157613147+avan-codes@users.noreply.github.com>
2026-03-29 21:43:24 +0530

    Update API endpoint to production URL

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -6,7 +6,7 @@ import MarkdownRenderer from '../components/MarkdownRenderer'
 
 // const API = 'https://track-x-backend-8fgp2sf04-pranjalsaini369s-projects.vercel.app/api'
 
-const API = 'http://127.0.0.1:8000/api'
+const API = 'https://backend-latest-21tb.onrender.com/api'
 
 
 const features = [
